95e3c6e999585406d3461e2d4d1084d02e07d8f1
jjdelcerro <jjdelcerro@administrador-Lenovo-G50-70>
2020-05-06 10:25:42 +0200

    Atrapados errores del autorun para que no intente hacer nada si no estan instalados los plugins de los que depende.

--- autorun.py
+++ autorun.py
@@ -1,15 +1,18 @@
 # encoding: utf-8
 
 import gvsig
-from addons.GeometriesTo2D.geometriesTo2D import TransformGeometriesTo2D
 from org.gvsig.tools import ToolsLocator
 from java.io import File
-from gvsig import uselib
-uselib.use_plugin("org.gvsig.toolbox")
 
-def main(*args):
-    selfRegister()
-    
+try:
+  from addons.GeometriesTo2D.geometriesTo2D import TransformGeometriesTo2D
+except:
+  import sys
+  ex = sys.exc_info()[1]
+  gvsig.logger("Can't load module 'TransformGeometriesTo2D'. " + str(ex), gvsig.LOGGER_WARN)#, ex)
+  TransformGeometriesTo2D = None
+
+
 def selfRegister(*args):
     i18nManager = ToolsLocator.getI18nManager()
     i18nManager.addResourceFamily("text",File(gvsig.getResource(__file__,"i18n")))
@@ -18,4 +21,9 @@ def selfRegister(*args):
     process.selfregister("Scripting")
     process.updateToolbox()
 
+def main(*args):
+    if TransformGeometriesTo2D== None:
+      return
+    selfRegister()
+    
     
